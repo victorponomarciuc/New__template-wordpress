755019b81461bd1f6edaefe4f2b071c37140713a
Victor Ponomarciuc <olga.2000.cristiuc@gmail.com>
2021-02-20 20:19:46 +0200

    new section+adaptiv

---
+++ README.md
@@ -0,0 +1,1 @@
+# New__template-wordpress
---
+++ assets/js/custom.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    var show = true;
+    var countbox = ".counter__inner";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.counter__number').css('opacity', '1');
+            $('.counter__number').spincrement({
+                thousandSeparator: "",
+                duration: 2000
+            });
+
+            show = false;
+        }
+    });
+
+});
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+//carousel
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block"; 
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
